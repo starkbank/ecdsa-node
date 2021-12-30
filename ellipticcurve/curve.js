fcf76d183fdaea214a184f23a2b3eef80454703d
roschnor <35103237+roschnor@users.noreply.github.com>
2021-12-30 10:39:04 +0100

    Update curve.js

--- ellipticcurve/curve.js
+++ ellipticcurve/curve.js
@@ -63,7 +63,7 @@ let prime256v1 = new CurveFp(
     BigInt("6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296", 16),
     BigInt("4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5", 16),
     "prime256v1",
-    [1, 2, 840, 10045, 3, 1, 7],
+    [1, 2, 840, 10045, 3, 1, 1, 7],
     "P-256"
 );
 
